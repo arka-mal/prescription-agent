b6949b3649db8e1e8fd7f3f570c35925f9523492
arka-mal <arkadiptamal@gmail.com>
2026-08-29 02:08:12 +0530

    updated config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ USE_BACKEND_GCP_CREDS = True
 
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 GCP_SERVICE_ACCOUNT_INFO = dict(st.secrets["gcp_service_account"])
-GROQ_MODEL_DEFAULT = "llama-3.3-70b-versatile"
+GROQ_MODEL_DEFAULT = "openai/gpt-oss-120b"
